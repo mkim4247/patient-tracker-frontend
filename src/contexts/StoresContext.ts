ce2df6eaeb29e6d359725b5765656b831d55355d
mkim4247 <m.kim4247@gmail.com>
2020-12-26 18:00:01 -0500

    adding types

--- src/contexts/StoresContext.ts
+++ src/contexts/StoresContext.ts
@@ -1,6 +1,6 @@
 import { createContext } from "react";
 
-import UserStore from "stores/userStore";
+import UserStore from "../stores/userStore";
 
 const StoresContext = createContext({
   userStore: new UserStore(),
---
+++ src/hooks/stores/userStore.ts
@@ -0,0 +1,18 @@
+import { observable, action, set } from "mobx";
+
+import { User, DEFAULT_USER } from "types";
+
+export default class UserStore {
+  @observable
+  user: User = DEFAULT_USER;
+
+  @action
+  updateUser(newUser: Partial<User>) {
+    set(this.user, newUser);
+  }
+
+  @action
+  resetUser() {
+    this.updateUser(DEFAULT_USER);
+  }
+}

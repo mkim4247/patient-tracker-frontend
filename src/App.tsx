d006f9f2f61bf48d9ad34243602bca86d31a3a37
mkim4247 <m.kim4247@gmail.com>
2021-01-09 18:04:21 -0500

    rework App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,42 @@
 import React from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
+import { IonReactRouter } from "@ionic/react-router";
+import { IonApp, IonRouterOutlet, IonSplitPane } from "@ionic/react";
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <IonApp>
+      <IonReactRouter>
+        <Switch>
+          <IonSplitPane>
+            <Route
+              exact
+              path="/home"
+              render={() => {
+                return (
+                  <div className="App">
+                    <header className="App-header">
+                      <p>
+                        Edit <code>src/App.js</code> and save to reload.
+                      </p>
+                      <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Learn React
+                      </a>
+                    </header>
+                  </div>
+                );
+              }}
+            />
+          </IonSplitPane>
+        </Switch>
+      </IonReactRouter>
+    </IonApp>
   );
-}
+};
 
 export default App;

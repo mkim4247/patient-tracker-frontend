9130f069e93073b3aeeb61c46f9a0873b65f41a0
mkim4247 <m.kim4247@gmail.com>
2021-01-26 23:45:06 -0500

    adding login

---
+++ src/pages/Login.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { RouteComponentProps } from "react-router-dom";
+import { IonPage, IonLabel } from "@ionic/react";
+
+import useStores from "hooks/useStores";
+
+const Login: React.FC<RouteComponentProps> = ({ history }) => {};

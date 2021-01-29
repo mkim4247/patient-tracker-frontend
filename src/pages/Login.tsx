7c2f8c91ff26f58d178651ebbe48a539d6f7a997
mkim4247 <m.kim4247@gmail.com>
2021-01-29 15:38:34 -0500

    login form

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,5 +3,16 @@ import { RouteComponentProps } from "react-router-dom";
 import { IonPage, IonLabel } from "@ionic/react";
 
 import useStores from "hooks/useStores";
+import LoginForm from "forms/LoginForm";
 
-const Login: React.FC<RouteComponentProps> = ({ history }) => {};
+const Login: React.FC<RouteComponentProps> = ({ history }) => {
+  const { userStore } = useStores();
+
+  return (
+    <IonPage>
+      <LoginForm />
+    </IonPage>
+  );
+};
+
+export default Login;

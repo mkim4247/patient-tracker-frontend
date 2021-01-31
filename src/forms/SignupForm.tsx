7bf1988c0d42d6246e0135379f94ed7c7fb17d1e
mkim4247 <m.kim4247@gmail.com>
2021-01-31 16:57:17 -0500

    Signup Form

--- src/forms/SignupForm.tsx
+++ src/forms/SignupForm.tsx
@@ -1,1 +1,54 @@
 import React from "react";
+import { withFormik, FormikProps, FormikBag, Field } from "formik";
+import {
+  IonButton,
+  IonContent,
+  IonFooter,
+  IonItem,
+  IonLabel,
+} from "@ionic/react";
+
+import * as Yup from "yup";
+
+import { User, SignupFormValues } from "types";
+import Signup from "pages/Signup";
+
+interface SignupFormProps {
+  user: User;
+  onSubmit: (values: SignupFormValues) => void;
+}
+
+const SignupForm: React.FC<SignupFormProps & FormikProps<SignupFormValues>> = ({
+  isValid,
+  submitForm,
+}) => {
+  return (
+    <>
+      <IonContent></IonContent>
+    </>
+  );
+};
+
+export default withFormik<SignupFormProps, SignupFormValues>({
+  displayName: "SignupForm",
+  enableReinitialize: true,
+
+  validationSchema: Yup.object().shape({
+    firstName: Yup.string().required(),
+  }),
+
+  mapPropsToValues({ user }: SignupFormProps): SignupFormValues {
+    return user as SignupFormValues;
+  },
+
+  handleSubmit(
+    values: SignupFormValues,
+    {
+      props: { onSubmit },
+      setSubmitting,
+    }: FormikBag<SignupFormProps, SignupFormValues>
+  ) {
+    onSubmit(values);
+    setSubmitting(false);
+  },
+})(SignupForm);
